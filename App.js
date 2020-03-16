9b3b5b5e413ce9cddaa4385d207f30b28ad8da36
jasser97 <59313928+jasser97@users.noreply.github.com>
2020-03-16 21:47:52 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar1 from "./components/navbar";
+import Form1 from "./components/form";
+
+function App() {
+  return (
+    <form className="App">
+      <Navbar1 />
+      <Form1 />
+    </form>
+  );
+}
+
+export default App;
